b17f0d7e64a9e7ffda0ed7df2d330945feb04cad
Mckz33 <mackenzie.max.rj@gmail.com>
2023-11-17 09:14:30 -0300

    primeiro commit

---
+++ src/main/java/com/ger_professores/sistema/SistemaApplication.java
@@ -0,0 +1,13 @@
+package com.ger_professores.sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ger_professores/sistema/SistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ger_professores.sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
