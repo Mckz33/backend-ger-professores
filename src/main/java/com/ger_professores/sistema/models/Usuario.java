c3a0113797b8040b48b102c745cd080eae832738
Mckz33 <mackenzie.max.rj@gmail.com>
2023-12-02 19:27:32 -0300

    novos ajustes nas classes

--- src/main/java/com/ger_professores/sistema/models/Usuario.java
+++ src/main/java/com/ger_professores/sistema/models/Usuario.java
@@ -2,7 +2,6 @@ package com.ger_professores.sistema.models;
 
 import com.ger_professores.sistema.enums.Contratacao;
 import com.ger_professores.sistema.enums.Tipo;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -10,10 +9,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.io.Serializable;
 import java.util.List;
